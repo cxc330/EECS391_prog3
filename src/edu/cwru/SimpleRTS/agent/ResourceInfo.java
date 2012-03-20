ddf6b98dc818875b80b717024cd2f749b5edb25f
Chris Gross <cjg28@case.edu>
2012-03-20 16:48:36 -0300

    Update src/edu/cwru/SimpleRTS/agent/ResourceInfo.java

--- src/edu/cwru/SimpleRTS/agent/ResourceInfo.java
+++ src/edu/cwru/SimpleRTS/agent/ResourceInfo.java
@@ -1,10 +1,13 @@
 package edu.cwru.SimpleRTS.agent;
 
+import edu.cwru.SimpleRTS.model.resource.ResourceNode.Type;
+
 public class ResourceInfo {
 	public int collected = 0;
 	public int totalAvailable = 0;
 	public Integer x;
 	public Integer y;
+	public Type type;
 	
 	public ResourceInfo (int resourceValue)
 	{
