5fa236703c444e1753359323f7241f71341b9a92
Jeff <chien-hung.chen@case.edu>
2012-03-20 19:53:09 -0400

    added converting to action

--- src/edu/cwru/SimpleRTS/agent/PEAgent.java
+++ src/edu/cwru/SimpleRTS/agent/PEAgent.java
@@ -12,6 +12,9 @@ public class PEAgent extends Agent {
 	static int playernum = 0;
 	static String townHall = "TownHall";
 	static String peasant = "Peasant";
+	static String move = "move";
+	static String gather = "gather";
+	static String deposit = "deposit";
 	private int finalGoldTally = 200;
 	private int finalWoodTally = 200;
 	private boolean canBuildPeasant = false;
@@ -28,7 +31,7 @@ public class PEAgent extends Agent {
 	@Override
 	public Map<Integer, Action> middleStep(StateView state) 
 	{
-		ArrayList<STRIP> actions = new ArrayList<STRIP>();
+		Map<Integer, Action> actions = new HashMap<Integer, Action>();
 		List<Integer> allUnitIds = state.getAllUnitIds();
 		List<Integer> peasantIds = findUnitType(allUnitIds, state, peasant);
 		List<Integer> townHallIds = findUnitType(allUnitIds, state, townHall);
@@ -38,7 +41,7 @@ public class PEAgent extends Agent {
 		
 		if	(townHallIds.size() > 0) //TownHall Exists. Check if resources available in here too?
 		{
-			actions = planner.generatePlan(peasantIds.get(0), townHallIds.get(0), state);
+			actions = convertArrayListToMap(planner.generatePlan(peasantIds.get(0), townHallIds.get(0), state));
 		}	
 		else
 		{
@@ -55,6 +58,27 @@ public class PEAgent extends Agent {
 	@Override
 	public void terminalStep(StateView state) {
 
+	}
+	
+	public Map<Integer, Action> convertArrayListToMap(ArrayList<STRIP> actionsIn)
+	{
+		Map<Integer, Action> actionsOut = new HashMap<Integer, Action>();
+		for(int i = 1; i < actionsIn.size(); i++)
+		{
+			if(actionsIn.get(i).unit.getTemplateView().getUnitName() == move)
+			{
+				actionsOut.put(actionsIn.get(i).unit.getID(), Action.createCompoundMove(actionsIn.get(i).unit.getID(), actionsIn.get(i).unit.getXPosition(), actionsIn.get(i).unit.getYPosition()));
+			}
+			else if(actionsIn.get(i).unit.getTemplateView().getUnitName() == deposit)
+			{
+				actionsOut.put(actionsIn.get(i).unit.getID(), Action.createCompoundDeposit(actionsIn.get(i).unit.getID(), actionsIn.get(i-1).unit.getID()));
+			}
+			else if(actionsIn.get(i).unit.getTemplateView().getUnitName() == gather)
+			{
+				actionsOut.put(actionsIn.get(i).unit.getID(), Action.createCompoundGather(actionsIn.get(i).unit.getID(), actionsIn.get(i-1).unit.getID()));
+			}
+		}
+		return actionsOut;
 	}
 	
 	public List<Integer> findUnitType(List<Integer> ids, StateView state, String name)
