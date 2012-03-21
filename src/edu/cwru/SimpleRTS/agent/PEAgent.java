64a457834bc0c54a3e10cab67e2462ca36371c18
Jeff <chien-hung.chen@case.edu>
2012-03-20 21:21:26 -0400

    commit to pull

--- src/edu/cwru/SimpleRTS/agent/PEAgent.java
+++ src/edu/cwru/SimpleRTS/agent/PEAgent.java
@@ -83,7 +83,20 @@ public class PEAgent extends Agent {
 		if(actionsIn.unit.getTemplateView().getUnitName() == move)
 		{
 			isMoving = true;
-			actionsOut.put(pID, Action.createCompoundMove(pID, actionsIn.unit.getXPosition(), actionsIn.unit.getYPosition()));
+			//actionsOut.put(pID, Action.createCompoundMove(pID, actionsIn.unit.getXPosition(), actionsIn.unit.getYPosition()));
+			if(state.unitAt(actionsIn.unit.getXPosition(), actionsIn.unit.getYPosition()) != null)
+			{
+				actionsOut = astar.aStarSearch(pID, state.unitAt(actionsIn.unit.getXPosition(), actionsIn.unit.getYPosition()), state);
+			}
+			else if(state.resourceAt(actionsIn.unit.getXPosition(), actionsIn.unit.getYPosition()) != null)
+			{
+				actionsOut = astar.aStarSearch(pID, state.resourceAt(actionsIn.unit.getXPosition(), actionsIn.unit.getYPosition()), state);
+			}
+			
+			if(actionsOut == null)
+			{
+				actionsOut = new HashMap<Integer, Action>();
+			}
 		}
 		else if(actionsIn.unit.getTemplateView().getUnitName() == deposit)
 		{
